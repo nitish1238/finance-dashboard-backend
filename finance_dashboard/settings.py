dbd77c6b59a09375a1cd1d324581e78a456c28e5
nitish1238 <nitishkumaryadav2205@gmail.com>
2026-04-05 08:25:16 +0530

    removed secret key from settings

--- finance_dashboard/settings.py
+++ finance_dashboard/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-''' SECRET_KEY = django-insecure-z+0$m5a#i=dn5gb5i!i4!^*@h@bdm204xz1m3n)d4kro^%_=@q '''
+
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 
